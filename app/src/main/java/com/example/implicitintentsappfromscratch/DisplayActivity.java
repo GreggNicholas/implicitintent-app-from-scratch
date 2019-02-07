86a1163f9c423eec4805d29fe854df43fcd9196f
Gregg Nicholas <Greggnicholas@pursuit.org>
2019-02-06 23:00:26 -0500

    commits internet fix

--- app/src/main/java/com/example/implicitintentsappfromscratch/DisplayActivity.java
+++ app/src/main/java/com/example/implicitintentsappfromscratch/DisplayActivity.java
@@ -1,19 +1,25 @@
 package com.example.implicitintentsappfromscratch;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import java.net.URI;
+
 public class DisplayActivity extends AppCompatActivity {
     private TextView nameView;
     private TextView dateView;
     private ImageView imageView;
     private Button button;
+
+    private final String ADDRESS = "https://www.astrology.com/horoscope/daily/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +28,13 @@ public class DisplayActivity extends AppCompatActivity {
         nameView = findViewById(R.id.zodiacname_displayactivity);
         dateView = findViewById(R.id.zodiacnumber_displayactivity);
         imageView = findViewById(R.id.zodiacimage_displayactivity);
+        button = findViewById(R.id.button_display);
+
 
         Intent intent = getIntent();
-        if (intent == null) {
-            String name = intent.getStringExtra("name");
+
+
+            final String name = intent.getStringExtra("name");
             String date = intent.getStringExtra("number");
             String image = intent.getStringExtra("image");
 
@@ -36,7 +45,14 @@ public class DisplayActivity extends AppCompatActivity {
                     .into(imageView);
 
 
-            startActivity(intent);
-        }
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Uri uri = Uri.parse(ADDRESS + name + ".html");
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+                }
+            });
+
     }
 }
